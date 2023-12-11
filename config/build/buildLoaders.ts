d63c36ff7bb7cb8c843b73311faa2dbe8a9bb6fc
ValpSerg <Ghostnord@gmail.com>
2023-12-11 20:47:14 +0200

    Vite add

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -16,11 +16,11 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
   const cssLoader = buildCssLoader(isDev);
 
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: "ts-loader",
-    exclude: /node_modules/,
-  };
+  // const typescriptLoader = {
+  //   test: /\.tsx?$/,
+  //   use: "ts-loader",
+  //   exclude: /node_modules/,
+  // };
 
   const fileLoader = {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
@@ -36,7 +36,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     svgLoader,
     codeBabelLoader,
     tsxCodeBabelLoader,
-    typescriptLoader,
+    // typescriptLoader,
     cssLoader,
   ];
 }
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        // ...
+      },
+
+      // esbuild options, to transform jsx to js
+      esbuildOptions: {
+        // ...
+      },
+
+      // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
+      include: "**/*.svg",
+
+      //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
+      exclude: "",
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  define: {
+    __IS_DEV__: JSON.stringify(true),
+    __API__: JSON.stringify("http://localhost:8000"),
+    __PROJECT__: JSON.stringify("frontend"),
+  },
+});
